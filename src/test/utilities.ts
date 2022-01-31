8bc3a7a93ac76ce99d0e1ec2227bf3b132e96f29
beefchimi <curtdulmage@gmail.com>
2022-01-31 17:07:49 -0500

    :construction_worker: [useEventListener] Convert tests

--- src/test/utilities.ts
+++ src/test/utilities.ts
@@ -2,7 +2,7 @@ import type {ReactElement} from 'react';
 import {render} from '@testing-library/react';
 
 export function mount(component: ReactElement, options = {}) {
-  render(component, {
+  return render(component, {
     // NOTE: Wrap provider(s) here if needed.
     wrapper: ({children}) => children,
     ...options,
