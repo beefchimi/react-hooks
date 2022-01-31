fb19c29e81bc3b87c2bc8faadaa7bfaa8ad2e8da
beefchimi <curtdulmage@gmail.com>
2022-01-31 15:33:24 -0500

    :wrench: [React] Configure testing library

---
+++ src/test/utilities.ts
@@ -0,0 +1,10 @@
+import type {ReactElement} from 'react';
+import {render} from '@testing-library/react';
+
+export function mount(component: ReactElement, options = {}) {
+  render(component, {
+    // NOTE: Wrap provider(s) here if needed.
+    wrapper: ({children}) => children,
+    ...options,
+  });
+}
