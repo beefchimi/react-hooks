13e5e99b97228788b2cee0fce7009cf126db98a6
beefchimi <curtdulmage@gmail.com>
2022-03-26 12:03:08 -0400

    :truck: [Vitest] Move test config folder

--- src/useKeyPress/tests/useKeyPress.test.tsx
+++ src/useKeyPress/tests/useKeyPress.test.tsx
@@ -3,7 +3,7 @@ import {screen} from '@testing-library/react';
 import {renderHook} from '@testing-library/react-hooks';
 import userEvent from '@testing-library/user-event';
 
-import {mount} from '../../test/utilities';
+import {mount} from '../../../test/utilities';
 import {useKeyPress} from '../useKeyPress';
 import {KeyPressEventType} from '../types';
 import type {KeyPressInput, KeyPressHookOptions} from '../types';
--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ const DTS_ENTRY_CONTENT = `export * from './types/index';`;
 const testConfig: vitestTypes.InlineConfig = {
   global: true,
   environment: 'jsdom',
-  setupFiles: './src/test/setup.ts',
+  setupFiles: './test/setup.ts',
 };
 
 // TODO: Do we actually need to include React DOM?
