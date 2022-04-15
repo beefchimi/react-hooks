afe069d7308b0814ca5eab36709e45ef22fb62e8
beefchimi <curtdulmage@gmail.com>
2022-04-15 17:26:44 -0400

    :hook: [useMounted] New hook

---
+++ src/useMounted/useMounted.ts
@@ -0,0 +1,15 @@
+import {useCallback, useEffect, useRef} from 'react';
+
+export function useMounted() {
+  const mounted = useRef(false);
+
+  useEffect(() => {
+    mounted.current = true;
+
+    return () => {
+      mounted.current = false;
+    };
+  }, []);
+
+  return useCallback(() => mounted.current, []);
+}
