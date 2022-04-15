b97a82bfebac88b08c80b1e930013d80ac7dd6e9
beefchimi <curtdulmage@gmail.com>
2022-04-15 15:47:27 -0400

    :wrench: [Test] Add new mountWithUser utility

--- src/test/utilities.ts
+++ src/test/utilities.ts
@@ -1,5 +1,6 @@
 import type {ReactElement} from 'react';
 import {render} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 export function mount(component: ReactElement, options = {}) {
   return render(component, {
@@ -7,4 +8,17 @@ export function mount(component: ReactElement, options = {}) {
     wrapper: ({children}) => children,
     ...options,
   });
+}
+
+// TODO: Is it really worth keeping this separate from `mount()`?
+// If we really want to avoid initializing `userEvent` when we don't need it,
+// then we can simply include `userEvent?: boolean` in the `options`.
+// TODO: Also, shall we do the same thing with `renderHook()`?
+export function mountWithUser(component: ReactElement, options = {}) {
+  const mountedComponent = mount(component, options);
+
+  return {
+    user: userEvent.setup(),
+    ...mountedComponent,
+  };
 }
