fb9776daf5a40415e66c73ead1ccc40d6d158e03
beefchimi <curtdulmage@gmail.com>
2022-06-03 09:13:16 -0400

    :recycle: [useMounted] Update all tests to be async

--- src/useMounted/tests/useMounted.test.tsx
+++ src/useMounted/tests/useMounted.test.tsx
@@ -3,14 +3,14 @@ import {renderHook} from '@testing-library/react-hooks';
 import {useMounted} from '../useMounted';
 
 describe('useMounted', () => {
-  it('is `true` upon `mount`', () => {
+  it('is `true` upon `mount`', async () => {
     const {result} = renderHook(() => useMounted());
     const isMounted = result.current;
 
     expect(isMounted()).toBe(true);
   });
 
-  it('is `false` upon `unmount`', () => {
+  it('is `false` upon `unmount`', async () => {
     const {result, unmount} = renderHook(() => useMounted());
     const isMounted = result.current;
 
