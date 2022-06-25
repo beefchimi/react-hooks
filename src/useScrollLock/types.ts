8affd25fa5832ada5e18e32fba383c84ce001a7e
beefchimi <curtdulmage@gmail.com>
2022-06-25 18:08:53 -0400

    :sparkles: [useScrollLock] Optimistically add new options

--- src/useScrollLock/types.ts
+++ src/useScrollLock/types.ts
@@ -5,7 +5,16 @@ import type {Dispatch, SetStateAction} from 'react';
 // It appears TypeScript is confused about array item positions.
 export type ScrollLockHookReturn = [boolean, Dispatch<SetStateAction<boolean>>];
 
+// eslint-disable-next-line @beefchimi/typescript/prefer-singular-enums
+export enum ScrollAxis {
+  Horizontal = 'x',
+  Vertical = 'y',
+  Both = 'both',
+}
+
 export interface ScrollLockOptions {
   target?: HTMLElement | null;
+  scrollAxis?: ScrollAxis;
   scrollbarOffset?: number;
+  bypassScrollbarFix?: boolean;
 }
