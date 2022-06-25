d8de6b9efef186552c590ce383649ad2c3421962
beefchimi <curtdulmage@gmail.com>
2022-06-25 17:30:20 -0400

    :hammer: [Utilities] Add new detect utilities

--- src/utilities/detect.ts
+++ src/utilities/detect.ts
@@ -5,3 +5,21 @@ export function detectHasDom() {
     typeof window.document.createElement !== 'undefined'
   );
 }
+
+export function detectIsAndroid() {
+  return navigator?.userAgent ? /android/i.test(navigator.userAgent) : false;
+}
+
+export function detectIsIpad() {
+  return (
+    navigator?.maxTouchPoints &&
+    navigator.maxTouchPoints > 2 &&
+    /MacIntel/.test(navigator.platform)
+  );
+}
+
+export function detectIsIos() {
+  return navigator?.platform
+    ? /iPad|iPhone|iPod/.test(navigator.platform) || detectIsIpad()
+    : false;
+}
