5cac2fd8cecbc066551a1507e93ed073f1b5e53f
beefchimi <curtdulmage@gmail.com>
2022-06-25 17:28:40 -0400

    :recycle: [Hooks] Update any hooks using canUseDom

--- src/useIsoLayoutEffect/useIsoLayoutEffect.ts
+++ src/useIsoLayoutEffect/useIsoLayoutEffect.ts
@@ -1,7 +1,7 @@
 import {useEffect, useLayoutEffect} from 'react';
 
-import {canUseDom} from '../utilities';
+import {detectHasDom} from '../utilities';
 
 // This "isomorphic" hook resolves to useEffect on the server
 // and useLayoutEffect on the client.
-export const useIsoLayoutEffect = canUseDom ? useLayoutEffect : useEffect;
+export const useIsoLayoutEffect = detectHasDom() ? useLayoutEffect : useEffect;
